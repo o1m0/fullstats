f26f4099480c9198a635615dcfad0ea214767800
o1m0 <dev@0ba.dev>
2026-03-23 23:17:33 +0900

    🎉chore: firstCommit
    ✨ feat(${1:scope}): ${2:message}
    
    refs #

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, '../config/.env') });
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.set("views", path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
